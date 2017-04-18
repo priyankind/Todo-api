ece0fa7ab3a4bb320a2f93414063b2c675a305b7
Priyank Gupta <kumarpriyankgupta@gmail.com>
2017-04-18 19:02:45 +0530

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -24,6 +24,22 @@ app.get('/todos', function(req, res){
 	res.json(todos);
 })
 
+app.get('/todos/:id', function(req,res){
+	var todoID= parseInt(req.params.id, 10);
+	var matchedTodo;
+	
+	todos.forEach(function(todo){
+		if(todoID === todo.id){
+			matchedTodo = todo;
+		}	
+	});
+	if(matchedTodo){
+		res.json(matchedTodo);
+	}else{
+		res.status(404).send();
+	}
+});
+
 app.listen(PORT, function(){
 	console.log('Express listening on port: '+PORT);
 })
