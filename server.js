641c2bc4941d497dbb56e05214249a9c2904fbb1
Priyank Gupta <kumarpriyankgupta@gmail.com>
2017-04-18 18:18:51 +0530

    added todos

--- server.js
+++ server.js
@@ -2,10 +2,27 @@ var express = require('express');
 var app = express();
 
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Meet me for lunch today!',
+	completed: false
+},{
+	id: 2,
+	description: 'Go to market and buy some mushrooms!',
+	completed: false
+},{
+	id: 3,
+	description:'I had Salad today for lunch!',
+	completed: true
+}];
 
 app.get('/', function(req, res){
 	res.send('Todo API root!');
 });
+
+app.get('/todos', function(req, res){
+	res.json(todos);
+})
 
 app.listen(PORT, function(){
 	console.log('Express listening on port: '+PORT);
