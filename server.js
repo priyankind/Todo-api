048528b248d25dd469d56fadbb0eeeebd09465b2
Priyank Gupta <kumarpriyankgupta@gmail.com>
2017-04-20 21:03:02 +0530

    Added PUT todos

--- server.js
+++ server.js
@@ -46,6 +46,48 @@ app.get('/todos/:id', function(req, res) {
 	}
 });
 
+app.delete('/todos/:id', function(req,res){
+	var todoID = parseInt(req.params.id,10);
+	var matchedTodo = _.findWhere(todos,{
+		id: todoID
+	});
+	if(!matchedTodo){
+		res.status(404).json({"error": "no todo with that id exists"});
+	}else{
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+});
+
+app.put('/todos/:id',function(req, res){
+	var todoID = parseInt(req.params.id,10);
+	var matchedTodo = _.findWhere(todos,{
+		id: todoID
+	});
+	var body = _.pick(req.body,'description','completed');
+	var validAttributes = {};
+
+	if(!matchedTodo){
+		res.status(404).send();
+	}
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	}else if(body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	}
+
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.completed;
+	}else if(body.hasOwnProperty('description')){
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+	
+});
+
 app.listen(PORT, function() {
 	console.log('Express listening on port: ' + PORT);
 })
