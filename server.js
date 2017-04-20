77c640e44636113dd13729dda0eadf6eb4377772
Priyank Gupta <kumarpriyankgupta@gmail.com>
2017-04-20 19:52:23 +0530

    refactor api with underscore

--- server.js
+++ server.js
@@ -1,48 +1,51 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 
 var PORT = process.env.PORT || 3000;
 var todos = [];
-var todoNextId =1;
+var todoNextId = 1;
 
 app.use(bodyParser.json());
 
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 	res.send('Todo API root!');
 });
 
-app.get('/todos', function(req, res){
+app.get('/todos', function(req, res) {
 	res.json(todos);
 })
 
 
-app.post('/todos',function(req,res){
-	var body = req.body;
+app.post('/todos', function(req, res) {
+	var body = _.pick(req.body,'description','completed');
+
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+	body.description = body.description.trim();
 	body.id = todoNextId;
 	todoNextId++;
 	todos.push(body);
-	console.log('description: '+body.description);
 	res.json(body);
-});
-
-app.get('/todos/:id', function(req,res){
-	var todoID= parseInt(req.params.id, 10);
-	var matchedTodo;
 	
-	todos.forEach(function(todo){
-		if(todoID === todo.id){
-			matchedTodo = todo;
-		}	
-	});
-	if(matchedTodo){
-		res.json(matchedTodo);
-	}else{
-		res.status(404).send();
-	} 
 });
 
-app.listen(PORT, function(){
-	console.log('Express listening on port: '+PORT);
+app.get('/todos/:id', function(req, res) {
+	var todoID = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {
+		id: todoID
+	});
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+});
+
+app.listen(PORT, function() {
+	console.log('Express listening on port: ' + PORT);
 })
